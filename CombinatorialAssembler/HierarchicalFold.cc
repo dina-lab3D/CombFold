8cdd397e4c6f934f8c75319f7b96014cefd6a1dd
daniellezaccai <danielle.zaccai@mail.huji.ac.il>
2024-04-07 16:04:17 +0300

    asd

--- CombinatorialAssembler/HierarchicalFold.cc
+++ CombinatorialAssembler/HierarchicalFold.cc
@@ -337,7 +337,10 @@ float HierarchicalFold::computeNewTransScore(const SuperBB &sbb1, const SuperBB 
 //                        !sbb1.trans_[i] * !it.transformation() * sbb2.trans_[j],
                         it2.transformation()); //todo - operator-?
                 if (rmsdBetweenBBs < 3) { //todo: threshold
-                    std::cout<< "found extra interface" << std::endl;
+                    std::cout<< "found extra interface: " << rmsdBetweenBBs  << ", " <<
+                    sbb1.trans_[i]<<" " << it.transformation() << " " << sbb2.trans_[j] <<
+                    " " << !sbb1.trans_[i] * it.transformation() * sbb2.trans_[j] <<
+                    " " << it2.transformation() << std::endl;
                     total_score += it2.getScore();
                 }
             }
